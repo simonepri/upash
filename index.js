829ae9695024c32d30815a82f143165ce77ce061
simonepri <simone.pri@hotmail.it>
2017-09-05 16:47:15 +0200

    Use promises

--- index.js
+++ index.js
@@ -4,97 +4,95 @@ const funcs = {};
 
 /**
  * Creates a new 'unique' hash from a password.
+ * @public
  * @param  {string} password The password to hash.
  * @param  {object} options Options to configure the hash function.
- * @param  {hashCallback} callback Called after the hash has been computed.
+ * @param  {string} options.func The name of the hash function to use.
+ * @returns {Promise<string>} A promise that contains a stringified object
+ *  that holds the generated hash string, the name of the function used to hash
+ *  it.
  */
- /**
-  * @callback hashCallback
-  * @param  {object} err  Possible error thrown.
-  * @param  {string} hash Stringified hash object that holds the generated hash
-  *   string, the name of the function used and the parameters passed to the
-  *   hash funciton needed for the verification process.
-  */
-function toHash(password, options, callback) {
-  if (!options || !options.func) {
-    return callback(new Error('You must provide an hash function to use.'));
-  }
-  const hashFunc = funcs[options.func];
+function toHash(password, options) {
+  return new Promise((resolve, reject) => {
+    if (!options || !options.func) {
+      return reject(new Error('You must provide an hash function to use.'));
+    }
+    const hashFunc = funcs[options.func];
 
-  if (!hashFunc) {
-    return callback(new Error(options.func + ' hash function not installed.'));
-  } else if (typeof password !== 'string' || password.length === 0) {
-    return callback(new Error('Password must be a non-empty string.'));
-  }
-
-  hashFunc.hash(password, options, (err, hash) => {
-    if (err) {
-      return callback(err);
+    if (!hashFunc) {
+      return reject(new Error(options.func + ' hash function not installed.'));
+    } else if (typeof password !== 'string' || password.length === 0) {
+      return reject(new Error('Password must be a non-empty string.'));
     }
 
-    callback(null, JSON.stringify({
-      hash,
-      func: options.func
-    }));
+    hashFunc.hash(password, options)
+      .then(hash => {
+        resolve(JSON.stringify({
+          hash,
+          func: options.func
+        }));
+      })
+      .catch(reject);
   });
 }
 
 /**
  * Determines whether or not the user's input matches the stored password.
- *
+ * @public
  * @param  {string} hash Stringified hash object generated from this package.
  * @param  {string} input User's password input.
- * @param  {verifyCallback} callback Called after the verification process has
- *   been computed.
+ * @returns {Promise<boolean>} A promise that contains a boolean that is true if
+ *   if the hash computed for the input matches.
  */
- /**
-  * @callback verifyCallback
-  * @param  {object} err Possible error thrown.
-  * @param  {boolean} match True if the hash computed for the input matches.
-  */
-function verifyHash(hash, input, callback) {
-  let hashObj;
-  try {
-    hashObj = JSON.parse(hash);
-  } catch (err) {
-    return callback(new Error('Couldn\'t parse the provided hash.'));
-  }
-  if (typeof hashObj.hash !== 'string' || typeof hashObj.func !== 'string' || hashObj.hash === '' || hashObj.func === '') {
-    return callback(new Error('The provided hash is invalid.'));
-  }
+function verifyHash(hash, input) {
+  return new Promise((resolve, reject) => {
+    let hashObj;
+    try {
+      hashObj = JSON.parse(hash);
+    } catch (err) {
+      return reject(new Error('Couldn\'t parse the provided hash.'));
+    }
+    if (typeof hashObj.hash !== 'string' || typeof hashObj.func !== 'string' || hashObj.hash === '' || hashObj.func === '') {
+      return reject(new Error('The provided hash is invalid.'));
+    }
 
-  const hashFunc = funcs[hashObj.func];
+    const hashFunc = funcs[hashObj.func];
 
-  if (!hashFunc) {
-    return callback(new Error(hashObj.func + ' hash function not installed.'));
-  } else if (typeof (input) !== 'string' || input.length === 0) {
-    return callback(new Error('Input password must be a non-empty string.'));
-  }
+    if (!hashFunc) {
+      return reject(new Error(hashObj.func + ' hash function not installed.'));
+    } else if (typeof (input) !== 'string' || input.length === 0) {
+      return reject(new Error('Input password must be a non-empty string.'));
+    }
 
-  hashFunc.verify(hashObj.hash, input, callback);
+    hashFunc.verify(hashObj.hash, input)
+      .then(resolve)
+      .catch(reject);
+  });
 }
 
 /**
- * Installs an hash function plugin.
- * @param {object} plugin A plugin compatible with this package.
+ * Installs an hash function hashFunction.
+ * @public
+ * @param {object} hashFunction An hash function compatible with this package.
  */
-function installPlugin(plugin) {
-  funcs[plugin.name] = {
-    hash: plugin.hash,
-    verify: plugin.verify
+function installFunction(hashFunction) {
+  funcs[hashFunction.name] = {
+    hash: hashFunction.hash,
+    verify: hashFunction.verify
   };
 }
 
 /**
+ * @public
  * @return {array} The array of the available hash functions.
  */
-function listPlugins() {
+function listFunctions() {
   return Object.keys(funcs);
 }
 
 module.exports = {
   verify: verifyHash,
   hash: toHash,
-  install: installPlugin,
-  plugins: listPlugins
+  install: installFunction,
+  list: listFunctions
 };
--- test.js
+++ test.js
@@ -1,22 +1,25 @@
 import test from 'ava';
-import pify from 'pify';
 
 import m from '.';
 
 const ponyHash = {
-  hash: (password, options, callback) => {
-    if (password === 'pony') {
-      callback(new Error('Cannot hash pony passwords'));
-      return;
-    }
-    callback(null, password);
+  hash: password => {
+    return new Promise((resolve, reject) => {
+      if (password === 'pony') {
+        reject(new Error('Cannot hash pony passwords'));
+        return;
+      }
+      resolve(password);
+    });
   },
-  verify: (hash, password, callback) => {
-    if (password === 'pony') {
-      callback(new Error('Cannot verify pony passwords'));
-      return;
-    }
-    callback(null, hash === password);
+  verify: (hash, password) => {
+    return new Promise((resolve, reject) => {
+      if (password === 'pony') {
+        reject(new Error('Cannot verify pony passwords'));
+        return;
+      }
+      resolve(hash === password);
+    });
   },
   name: 'pony'
 };
@@ -24,64 +27,64 @@ const ponyHash = {
 m.install(ponyHash);
 
 test('should verify a correct password', async t => {
-  const hash = await pify(m.hash)('hello world', {func: 'pony'});
+  const hash = await m.hash('hello world', {func: 'pony'});
   t.true(typeof hash === 'string');
-  t.true(await pify(m.verify)(hash, 'hello world'));
+  t.true(await m.verify(hash, 'hello world'));
 });
 
 test('should not verify a wrong password', async t => {
-  const hash = await pify(m.hash)('Hello world', {func: 'pony'});
+  const hash = await m.hash('Hello world', {func: 'pony'});
   t.true(typeof hash === 'string');
-  t.false(await pify(m.verify)(hash, 'hello world'));
+  t.false(await m.verify(hash, 'hello world'));
 });
 
 test('should throw an error hasing a pony password', async t => {
-  const err = await t.throws(pify(m.hash)('pony', {func: 'pony'}));
+  const err = await t.throws(m.hash('pony', {func: 'pony'}));
   t.is(err.message, 'Cannot hash pony passwords');
 });
 
 test('should throw an error verifying a pony password', async t => {
-  const hash = await pify(m.hash)('hello world', {func: 'pony'});
-  const err = await t.throws(pify(m.verify)(hash, 'pony'));
+  const hash = await m.hash('hello world', {func: 'pony'});
+  const err = await t.throws(m.verify(hash, 'pony'));
   t.is(err.message, 'Cannot verify pony passwords');
 });
 
 test('should throw an error tring to hash an empty string', async t => {
-  const err = await t.throws(pify(m.hash)('', {func: 'pony'}));
+  const err = await t.throws(m.hash('', {func: 'pony'}));
   t.is(err.message, 'Password must be a non-empty string.');
 });
 
 test('should throw an error tring to verify an empty string', async t => {
-  const hash = await pify(m.hash)('hello world', {func: 'pony'});
-  const err = await t.throws(pify(m.verify)(hash, ''));
+  const hash = await m.hash('hello world', {func: 'pony'});
+  const err = await t.throws(m.verify(hash, ''));
   t.is(err.message, 'Input password must be a non-empty string.');
 });
 
 test('should throw an error tring to verify with and invalid hash', async t => {
-  let err = await t.throws(pify(m.verify)('invalid hash', 'hello world'));
+  let err = await t.throws(m.verify('invalid hash', 'hello world'));
   t.is(err.message, 'Couldn\'t parse the provided hash.');
-  err = await t.throws(pify(m.verify)('{}', 'hello world'));
+  err = await t.throws(m.verify('{}', 'hello world'));
   t.is(err.message, 'The provided hash is invalid.');
 });
 
 test('should throw an error tring to hash with an hash function not installed', async t => {
-  const err = await t.throws(pify(m.hash)('strong password', {func: 'unicorn'}));
+  const err = await t.throws(m.hash('strong password', {func: 'unicorn'}));
   t.is(err.message, 'unicorn hash function not installed.');
 });
 
 test('should throw an error tring to verify with an hash function not installed', async t => {
   const hash = JSON.stringify({hash: 'strong password', func: 'unicorn'});
-  const err = await t.throws(pify(m.verify)(hash, 'strong password'));
+  const err = await t.throws(m.verify(hash, 'strong password'));
   t.is(err.message, 'unicorn hash function not installed.');
 });
 
 test('should throw an error tring to hash without passing an hash function to use', async t => {
-  const err = await t.throws(pify(m.hash)('strong password', {}));
+  const err = await t.throws(m.hash('strong password', {}));
   t.is(err.message, 'You must provide an hash function to use.');
 });
 
-test('should return the list of plugins installed', t => {
-  const list = m.plugins();
+test('should return the list of hash functions installed', t => {
+  const list = m.list();
   t.is(list.length, 1);
   t.is(list[0], 'pony');
 });
